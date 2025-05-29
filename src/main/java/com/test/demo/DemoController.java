a556ff3258260265a0f2554eb26db4ed2c113c02
mmadel <mohammed.adel.abo.foutouh@gmail.com>
2025-05-29 14:05:06 +0300

    init commit

---
+++ src/main/java/com/test/demo/DemoController.java
@@ -0,0 +1,16 @@
+package com.test.demo;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin
+@RestController
+@RequestMapping("ping")
+public class DemoController {
+    @GetMapping()
+    @ResponseBody
+    public ResponseEntity<String> getAll() {
+        return ResponseEntity.ok()
+                .body("Ping...!!!!");
+    }
+}

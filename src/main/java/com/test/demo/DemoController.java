9f8851ccf7e821eb43e76fd2fb340fa1e8180ddb
mmadel <mohammed.adel.abo.foutouh@gmail.com>
2025-06-26 18:05:00 +0300

    dummy commit

--- src/main/java/com/test/demo/DemoController.java
+++ src/main/java/com/test/demo/DemoController.java
@@ -17,7 +17,7 @@ public class DemoController {
     @ResponseBody
     public ResponseEntity<String> getAll() {
         return ResponseEntity.ok()
-                .body("I so am happy...!!!!");
+                .body("Last Ping ...!!!!");
     }
 
     @PostMapping("create")
